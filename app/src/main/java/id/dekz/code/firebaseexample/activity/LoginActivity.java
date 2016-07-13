0952f8031e482bbda405382c5a7be9c9c3e15939
Adi Andrea <dekz.itfz@gmail.com>
2016-07-13 17:04:51 +0800

    implementation register proccess

--- app/src/main/java/id/dekz/code/firebaseexample/activity/LoginActivity.java
+++ app/src/main/java/id/dekz/code/firebaseexample/activity/LoginActivity.java
@@ -1,10 +1,13 @@
 package id.dekz.code.firebaseexample.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,6 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.etLoginEmail)EditText email;
     @BindView(R.id.etLoginPassword)EditText password;
     @BindView(R.id.btnLogin)Button login;
+    @BindView(R.id.tvRegister)TextView register;
 
     @Override
     protected void onCreate(Bundle bundle){
@@ -24,5 +28,15 @@ public class LoginActivity extends AppCompatActivity {
 
         this.getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reg = new Intent(LoginActivity.this, RegisterActivity.class);
+                reg.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(reg);
+                LoginActivity.this.finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/id/dekz/code/firebaseexample/activity/RegisterActivity.java
@@ -0,0 +1,128 @@
+package id.dekz.code.firebaseexample.activity;
+
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import id.dekz.code.firebaseexample.MainActivity;
+import id.dekz.code.firebaseexample.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.etRegisterEmail)EditText email;
+    @BindView(R.id.etRegisterPassword)EditText password;
+    @BindView(R.id.etRegisterPasswordConfirm)EditText passwordConfirm;
+    @BindView(R.id.btnRegister)Button register;
+    @BindView(R.id.tvLogin)TextView login;
+
+    private FirebaseAuth auth;
+    private ProgressDialog progressRegister;
+
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+
+        auth = FirebaseAuth.getInstance();
+
+        this.getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(RegisterActivity.this, LoginActivity.class);
+                login.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(login);
+                RegisterActivity.this.finish();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.getText().length()==0 ||
+                        password.getText().length()==0 ||
+                        passwordConfirm.getText().length()==0){
+                    Toast.makeText(RegisterActivity.this, "Please Fill All Fields!", Toast.LENGTH_SHORT).show();
+                }else{
+                    String strEmail = email.getText().toString();
+                    Pattern emailPattern = Pattern.compile(EMAIL_PATTERN);
+                    Matcher emailMatcher = emailPattern.matcher(strEmail);
+
+                    if(emailMatcher.matches()){
+                        //email valid
+                        String pass = password.getText().toString();
+                        String passCOnfirm = passwordConfirm.getText().toString();
+                        if(pass.length()<6){
+                            Toast.makeText(RegisterActivity.this, "Password should be at least 6 characters", Toast.LENGTH_SHORT).show();
+                        }else{
+                            if(pass.equals(passCOnfirm)){
+                                registerProccess(strEmail,pass);
+                            }else{
+                                Toast.makeText(RegisterActivity.this, "Passwords Doesn't Match!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }else{
+                        Toast.makeText(RegisterActivity.this, "Email Format Not Valid!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void registerProccess(String email, String pass){
+        progressRegister = ProgressDialog.show(
+                this,
+                "Registering","register in progress, please wait..",
+                true,
+                false);
+
+        auth.createUserWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressRegister.dismiss();
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Register Success!", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Register Failed! "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(RegisterActivity.this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressRegister.dismiss();
+                        Log.d("authFailure",e.getMessage());
+                        //Toast.makeText(RegisterActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
