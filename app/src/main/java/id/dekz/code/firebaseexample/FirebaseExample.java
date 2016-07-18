e7e2a3ca2989e67b5fbc1ad4c756f04406c2927a
Adi Andrea <dekz.itfz@gmail.com>
2016-07-18 17:06:23 +0800

    impl crash reporting

--- app/src/main/java/id/dekz/code/firebaseexample/FirebaseExample.java
+++ app/src/main/java/id/dekz/code/firebaseexample/FirebaseExample.java
@@ -3,6 +3,7 @@ package id.dekz.code.firebaseexample;
 import android.app.Application;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.crash.FirebaseCrash;
 
 
 public class FirebaseExample extends Application {
--- app/src/main/java/id/dekz/code/firebaseexample/MainActivity.java
+++ app/src/main/java/id/dekz/code/firebaseexample/MainActivity.java
@@ -3,12 +3,16 @@ package id.dekz.code.firebaseexample;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.google.firebase.crash.FirebaseCrash;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,8 +21,10 @@ import id.dekz.code.firebaseexample.util.Session;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private Session session;
     @BindView(R.id.adView)AdView mAdView;
+    @BindView(R.id.btnSendCrash)Button sendCrash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544/6300978111");
         session = new Session(MainActivity.this);
+
+        sendCrash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseCrash.report(new Exception("Error sent from FirebaseExample App"));
+                Log.d(TAG,"isSingletonInitialized-> "+String.valueOf(FirebaseCrash.isSingletonInitialized()));
+            }
+        });
 
     }
 
